1ff28c75a851947bb74014b02b50280bbf59b460
reginasubtil <reginssubtil@gmail.com>
2025-11-25 19:16:03 -0300

    Add files via upload

---
+++ ContatosController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AgendaTelefonica.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AgendaTelefonica.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatosController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public ContatosController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Contato> Get()
+        {
+            return _context.Contatos.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Contato contato)
+        {
+            _context.Contatos.Add(contato);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(Get), new { id = contato.Id }, contato);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+                return NotFound();
+
+            _context.Contatos.Remove(contato);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
+
---
+++ Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaTelefonica
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<AgendaTelefonica.Models.AgendaContext>(options =>
+    options.UseSqlite("Data Source=agenda.db"));
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
